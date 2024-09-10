650e5a40cd778a1c9d7dc2357fd782a0c5c6d75b
mdsami98 <mdsami9898@gmail.com>
2024-09-11 00:25:48 +0600

    01 basic 1 to 4 done

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,16 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+    const operatorOptions = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '/': (a, b) => a / b,
+        '*': (a, b) => a * b
+    };
+
+    const calCulation = operatorOptions[operator];
+    if (calCulation) {
+        return calCulation(num1, num2);
+    }
+    return 0;
+}
 
 module.exports = calculator;
--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,13 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+    const stringArray = Array.from(str);
+    const count = stringArray.reduce((value, currentValue) => {
+        if (currentValue == char) {
+            return value + 1;
+        }
+        return value;
+    }, 0);
+
+    return count;
+}
 
 module.exports = countOccurrences;
